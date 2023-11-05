95cd4c271587e704b8b12d93158f586939eb53d6
Aaron Parker <aaron@stealthpuppy.com>
2023-11-05 14:14:31 +1100

    Update GetExeFilesToDenyList.ps1

--- AaronLocker/CustomizationInputs/GetExeFilesToDenyList.ps1
+++ AaronLocker/CustomizationInputs/GetExeFilesToDenyList.ps1
@@ -18,52 +18,55 @@
 
 # Find the multiple instances of .NET executables that have been identified as AllowList bypasses.
 # Create-Policies.ps1 will remove redundant information.
-$dotnetProgramsToDenyList =
-"AddInProcess.exe",
-"AddInProcess32.exe",
-"AddInUtil.exe",
-"aspnet_compiler.exe",
-"IEExec.exe",
-"InstallUtil.exe",
-"Microsoft.Build.dll",
-"Microsoft.Build.Framework.dll",
-"Microsoft.Workflow.Compiler.exe",
-"MSBuild.exe",
-"RegAsm.exe",
-"RegSvcs.exe",
-"System.Management.Automation.dll"
-$dotnetProgramsToDenyList | ForEach-Object {
-    Get-ChildItem -Path "$Env:SystemRoot\Microsoft.NET" -Recurse -Include $_ | ForEach-Object { $_.FullName }
-}
 
-# Additional Microsoft recommended executables to deny
-"Microsoft.Build.Framework.dll", "System.Management.Automation.dll" | ForEach-Object {
-    Get-ChildItem -Path "$Env:SystemRoot\assembly\GAC_MSIL" -Recurse -Include $_ | ForEach-Object { $_.FullName }
-}
+# https://learn.microsoft.com/en-us/windows/security/application-security/application-control/windows-defender-application-control/design/applications-that-can-bypass-wdac
 
-# Additional Microsoft recommended executables to deny
-"bash.exe", "wsl.exe", "wslconfig.exe", "wslhost.exe", "system.management.automation.dll", "lxssmanager.dll", "cscript.exe", "wscript.exe" | `
-    ForEach-Object {
-    Get-ChildItem -Path "$Env:SystemRoot\servicing\LCU" -Recurse -Include $_ -ErrorAction "Ignore" | ForEach-Object { $_.FullName }
-}
-
-@("$Env:SystemRoot\System32\mshta.exe",
-    "$Env:SystemRoot\System32\PresentationHost.exe",
-    "$Env:SystemRoot\System32\wbem\WMIC.exe") | ForEach-Object { Get-ChildItem -Path $_ | ForEach-Object { $_.FullName } }
-# Note: also need Code Integrity rules to block other bypasses
-
-# --------------------------------------------------------------------------------
-# Files used by ransomware / lolbins
-@("$Env:SystemRoot\System32\cipher.exe",
-    "$Env:SystemRoot\System32\certreq.exe",
-    "$Env:SystemRoot\System32\certutil.exe",
-    "$Env:SystemRoot\System32\Cmdl32.exe",
-    "$Env:SystemRoot\System32\msdt.exe") | ForEach-Object { Get-ChildItem -Path $_ | ForEach-Object { $_.FullName } }
-
-# --------------------------------------------------------------------------------
-# Block common credential exposure risk (also need to disable GUI option via registry, and SecondaryLogon service)
-@("$Env:SystemRoot\System32\runas.exe") | ForEach-Object { Get-ChildItem -Path $_ | ForEach-Object { $_.FullName } }
-
-# Block Scripting host
-@("$Env:SystemRoot\System32\cscript.exe",
-    "$Env:SystemRoot\System32\wscript.exe") | ForEach-Object { Get-ChildItem -Path $_ | ForEach-Object { $_.FullName } }
+@("addinprocess.exe",
+    "addinprocess32.exe",
+    "addinutil.exe",
+    "aspnet_compiler.exe",
+    "bash.exe",
+    "bginfo.exe",
+    "cdb.exe",
+    "cscript.exe",
+    "cipher.exe",
+    "certreq.exe",
+    "certutil.exe",
+    "Cmdl32.exe",
+    "msdt.exe"
+    "csi.exe",
+    "dbghost.exe",
+    "dbgsvc.exe",
+    "dnx.exe",
+    "dotnet.exe",
+    "fsi.exe",
+    "fsiAnyCpu.exe",
+    "infdefaultinstall.exe",
+    "kd.exe",
+    "kill.exe",
+    "lxssmanager.dll",
+    "lxrun.exe",
+    "Microsoft.Build.dll",
+    "Microsoft.Build.Framework.dll",
+    "Microsoft.Workflow.Compiler.exe",
+    "msbuild.exe2",
+    "msbuild.dll",
+    "mshta.exe",
+    "ntkd.exe",
+    "ntsd.exe",
+    "powershellcustomhost.exe",
+    "PresentationHost.exe",
+    "rcsi.exe",
+    "runas.exe",
+    "runscripthelper.exe",
+    "texttransform.exe",
+    "visualuiaverifynative.exe",
+    "system.management.automation.dll",
+    "wfc.exe",
+    "windbg.exe",
+    "wmic.exe",
+    "wscript.exe",
+    "wsl.exe",
+    "wslconfig.exe",
+    "wslhost.exe") | Get-ChildItem -Path "$Env:SystemRoot" -Include $Files -Recurse -ErrorAction "SilentlyContinue" | `
+    Select-Object -ExpandProperty "FullName" 
